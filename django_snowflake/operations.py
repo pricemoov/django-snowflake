98b9ac97fe75602e03816032da979f28825a58a1
Manan Thakkar <thakkar1016@gmail.com>
2021-12-18 20:27:03 -0500

    Adding support for datetime extraction QuerySet operations (Extract, ExtractMonth, etc.)

--- django_snowflake/operations.py
+++ django_snowflake/operations.py
@@ -1,6 +1,19 @@
+from django.conf import settings
 from django.db.backends.base.operations import BaseDatabaseOperations
 
-class DatabaseOperations(BaseDatabaseOperations):
 
+class DatabaseOperations(BaseDatabaseOperations):
     def quote_name(self, name):
         return '"%s"' % name.replace('.', '"."')
+
+    def _convert_field_to_tz(self, field_name, tzname):
+        if settings.USE_TZ:
+            field_name = "CONVERT_TIMEZONE('%s', 'UTC', %s)" % (tzname, field_name)
+        return field_name
+
+    def datetime_extract_sql(self, lookup_type, field_name, tzname):
+        field_name = self._convert_field_to_tz(field_name, tzname)
+        return self.date_extract_sql(lookup_type, field_name)
+
+    def date_extract_sql(self, lookup_type, field_name):
+        return "EXTRACT(%s FROM %s)" % (lookup_type.upper(), field_name)
